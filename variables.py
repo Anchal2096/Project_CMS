ea74cd1ccebae18b0a80b685639b6966a1a5fcbd
Dev Vrat <itsdvvs.18@gmail.com>
2018-10-15 17:35:23 +0530

    addition of colors for different purposes

---
+++ variables.py
@@ -0,0 +1,28 @@
+# list of colors which are not bounded to any specific widget type
+color = {
+    'darkGrey': '#2B2B2B',
+    'lightGrey': '#3C3F41',
+    'white': 'white',
+    'black': 'black',
+    'magenta': '#360539',
+    'normalPink': '#880565',
+    'lightPink': '#C809B5',
+}
+
+
+# Colors which are bounded to specific widget according to different themes
+# Theme 1: Ghost
+theme_ghost = {
+    'nav': color['lightGrey'],
+    'content': color['darkGrey'],
+    'font': color['white'],
+}
+
+# Theme 2: Fox
+theme_fox = {
+    'nav': color['normalPink'],
+    'content': color['magenta'],
+    'font': color['white'],
+}
+
+# Theme 3: ???
