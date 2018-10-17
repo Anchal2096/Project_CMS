c718185d9448404ffc7ebe13e51a5dcddc441c36
Dev Vrat <itsdvvs.18@gmail.com>
2018-10-17 23:22:56 +0530

    Remove static code for establishing connections

--- Database_Config.py
+++ Database_Config.py
@@ -1,24 +1,21 @@
 # Database connectivity establishment
 import sys
-import tkinter.messagebox as tm
 from pymongo import *
+from server_list import *
+import tkinter.messagebox as tm
 from pymongo.errors import ConnectionFailure
 
 # Connection of database server is global so that every function can use it
 
 # Getting connection to database server
-if_connected = MongoClient("localhost", 27017)
+mongo_connection = MongoClient(cloud_SVR['DEV'])
 
 """If connected then proceed further otherwise 
    connection failure is displayed in as a message 
-   (except will run) and program will exit with code 1
+   ('except' block will run) and program will exit with code 1
 """
 try:
-    if_connected.admin.command("ismaster")
+    mongo_connection.admin.command("ismaster")
 except ConnectionFailure:
     tm.showerror("DataBase Server not connected", ConnectionFailure)
     sys.exit(1)
-
-# setting a database handler (database object kind of thing)
-# you can change this database accordingly
-db = if_connected["University"]
